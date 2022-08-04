713941bb61cdfe1c50254ce8a41eb0f3e0766a64
Tony Balogh <tony.balogh@karrikintech.com>
2022-08-04 07:17:18 -0700

    Version of source after chapter 89

--- front-end/uploadImage.cmd
+++ front-end/uploadImage.cmd
@@ -1,2 +1,2 @@
-docker build -f front-end.dockerfile -t atonyba/front-end:1.0.1 .
-docker push atonyba/front-end:1.0.1
+docker build -f front-end.dockerfile -t atonyba/front-end:1.0.2 .
+docker push atonyba/front-end:1.0.2
