dc0a6ce22cc627980368b183f6825094f0317488
Tony Balogh <tony.balogh@karrikintech.com>
2022-08-05 09:23:39 -0700

    Final version of docker swarm version of containerization

--- project/uploadImage.cmd
+++ project/uploadImage.cmd
@@ -1,2 +1,2 @@
-docker build -f caddy.dockerfile -t atonyba/micro-caddy:1.0.1 .
-docker push atonyba/micro-caddy:1.0.1
+docker build -f caddy.dockerfile -t atonyba/micro-caddy:1.0.2 .
+docker push atonyba/micro-caddy:1.0.2
---
+++ project/uploadImageProd.cmd
@@ -0,0 +1,2 @@
+docker build -f caddy.production.dockerfile -t atonyba/micro-caddy-production:1.0.3 .
+docker push atonyba/micro-caddy-production:1.0.3
